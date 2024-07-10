9a836867d2da42d5fd7fe9d87a185809bca530ab
mrrsmburu <mrrsmburu@gmail.com>
2024-07-10 17:08:41 +0300

    commit assignment

--- lib/scraper.py
+++ lib/scraper.py
@@ -4,3 +4,7 @@ import requests
 
 headers = {'user-agent': 'my-app/0.0.1'}
 html = requests.get("https://flatironschool.com/", headers=headers)
+doc = BeautifulSoup(html.text, 'html.parser')
+courses = doc.select('.heading-50-black.color-black')
+for course in courses: 
+    print(course.contents[0].strip())	
